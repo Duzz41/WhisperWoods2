ba981369b9d507da379514f27c8831c41439e5ed
Duzz41 <osmansisman433@gmail.com>
2024-04-23 00:39:11 +0300

    Bug fixed vol 0.2

--- Assets/Osman/Scripts/OpenPanels.cs
+++ Assets/Osman/Scripts/OpenPanels.cs
@@ -9,24 +9,27 @@ public class OpenPanels : MonoBehaviour
 {
     private Animator _panelAnimator;
     public Panels[] _panelsIndex;
+    public GameObject[] _buttons;
 
-    public void Open(GameObject panel)
+    public void Open()
     {
-        _panelAnimator = panel.GetComponent<Animator>();
+        _panelAnimator = this.GetComponent<Animator>();
         _panelAnimator.SetTrigger("Open");
     }
 
     public void ChoosePanel(int i)
     {
         _panelAnimator = _panelsIndex[i]._panels[0].GetComponentInParent<Animator>();
-        
         _panelAnimator.SetTrigger("Open");
+        
+        foreach (GameObject b in _buttons)
+            b.GetComponent<Button>().interactable = false;
+        foreach (GameObject a in _panelsIndex[i]._panels)
+            a.SetActive(true);
+        DontOpen();
         EvntManager.TriggerEvent("IncreaseIndex");
-        for (int a = 0; a < _panelsIndex[i]._panels.Count; a++)
-        {
-            _panelsIndex[i]._panels[a].SetActive(true);
-        }
     }
+
 
     public void DontOpen()
     {
--- Assets/Osman/Scripts/PageManager.cs
+++ Assets/Osman/Scripts/PageManager.cs
@@ -14,16 +14,21 @@ public class PageManager : MonoBehaviour
     public List<GameObject> _panels = new List<GameObject>();
     public Animator _pageAnim;
     int _panelIndex = 0;
-    private OpenPanels _openPanels;
+    public List<OpenPanels> _openPanels;
+
     private ChangeManager _changePages;
     bool _pageChange = false;
-    public bool stopGame;
+
+
 
     void Start()
     {
         EvntManager.StartListening("NextQue", PanelManagment);
-        EvntManager.StartListening("IncreaseIndex",IncreasePanelIndex);
-        _openPanels = _panels[_panelIndex].GetComponent<OpenPanels>();
+        EvntManager.StartListening("IncreaseIndex", IncreasePanelIndex);
+
+        foreach (GameObject p in _panels)
+            _openPanels.Add(p.GetComponent<OpenPanels>());
+
         _changePages = GetComponentInParent<ChangeManager>();
         _pageAnim = GetComponent<Animator>();
     }
@@ -36,34 +41,31 @@ public class PageManager : MonoBehaviour
     public void PanelManagment()
     {
 
-        if (stopGame == false)
+        if (_pageChoises.Length == 1)
         {
-
-            if (_pageChoises.Length == 1)
+            if (_panelIndex != _panels.Count)
             {
-
-                if (_panelIndex != _panels.Count)
-                {
-                    _openPanels.Open(_panels[_panelIndex]);
-                    IncreasePanelIndex();
-                }
-                else
-                {
-                    stopGame = true;
-                    _changePages.ChangePage(_pageChoises[0]);
-                    _pageAnim.SetTrigger("Change");
-                }
+                _openPanels[_panelIndex].Open();
+                IncreasePanelIndex();
             }
             else
             {
-                if (_panelIndex != _panels.Count)
-                {
-                    _openPanels.Open(_panels[_panelIndex]);
-                    IncreasePanelIndex();
-                    
-                }
+                _changePages.ChangePage(_pageChoises[0]);
+                _pageAnim.SetTrigger("Change");
             }
+
         }
+        else
+        {
+
+            if (_panelIndex != _panels.Count)
+            {
+                _openPanels[_panelIndex].Open();
+                IncreasePanelIndex();
+            }
+
+        }
+
     }
 
     void IncreasePanelIndex()
